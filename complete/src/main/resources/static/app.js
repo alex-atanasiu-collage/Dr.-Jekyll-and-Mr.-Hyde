963f50bf989e2a92352b9b326822d530d328372f
adriana tufa <adriana tufa>
2016-11-09 22:02:29 +0200

    add image to character; ui improvements;

--- complete/src/main/resources/static/app.js
+++ complete/src/main/resources/static/app.js
@@ -6,12 +6,10 @@ var right = false;
 var down = false;
 var left = false;
 
-mapLenght = 1000;
-mapHeight = 1000;
-wallLat = 10;
-charLat = 4 * wallLat;
-
-var characters = [];
+var mapLenght = 1000;
+var mapHeight = 1000;
+var wallLat = 10;
+var charLat = 4 * wallLat;
 
 var gameInfo;
 
@@ -53,15 +51,15 @@ function connect() {
     });
 }
 
-
+// Send the move to the server
 function move(move) {
-    //TODO !!! continuous move ???
     stompClient.subscribe('/topic/game', function (game) {
         showGame(JSON.parse(game.body));
     });
     stompClient.send("/app/move", {}, JSON.stringify({'playerName': $("#name").val(), "move": move}));
 }
 
+// Response from server
 function showGame(game) {
     gameInfo = game;
     console.log(gameInfo);
@@ -71,29 +69,32 @@ function showGame(game) {
     if(nrOfPlayers < 4 && gameOn == false) {
         $("#game").append("<p> Wait for the other players to enter the game </p>")
     } else {
+        // show the scores; display them only once
+        if (!gameOn) {
+            for (var i = 0; i < nrOfPlayers; i++) {
+                $("#scores").append("<div class=\"col-xs-6 col-sm-3\"><div>" + game.playerList[i].name +
+                        "</div><div>Score: " + game.playerList[i].score + "</div> </div>");
+            }
+            $("#greetings").html("");
+        }
+
         gameOn = true;
         $("#game").append("<p> The game has started. </p>")
-        //TODO draw game.board and players with game.playerList[i].positionOx and Oy
     }
 
     for (var i = 0; i < nrOfPlayers; i++) {
-        $("#game").append("<p> player " + game.playerList[i].name + " </p>");
         if (allSprites.length <= i) {
-            var img = loadImage("pumpkin.png");
-            characters += createSprite((game.playerList[i].positionOx-2) * wallLat,
-                                       (game.playerList[i].positionOy-2) * wallLat,
-                                       charLat, charLat);
-            characters[i].addImage(img);
+            var img = loadImage("pumpkin_40x40.png");
+            var s = createSprite((game.playerList[i].positionOx - 2) * wallLat,
+                                 (game.playerList[i].positionOy - 2) * wallLat,
+                                 charLat, charLat);
+            s.addImage(img);
         }
         else {
-            console.log("personajul " + i);
-            console.log(characters[i]);
-            allSprites[i].position.x = (game.playerList[i].positionOx-2) * wallLat;
-            allSprites[i].position.y = (game.playerList[i].positionOy-2) * wallLat;
+            allSprites[i].position.x = (game.playerList[i].positionOx - 2) * wallLat;
+            allSprites[i].position.y = (game.playerList[i].positionOy - 2) * wallLat;
         }
     }
-
-    $("#game").append("<p> test -----> score 1st player " + game.playerList[0].score + " </p>");
 }
 
 $(function () {
@@ -103,6 +104,42 @@ $(function () {
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
 });
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.send("/app/bye", {}, JSON.stringify({'content': $("#name").val()}));
+        stompClient.send("/app/end", {}, JSON.stringify({'content': $("#name").val()}));
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+// Setup for drawing
+function setup() {
+	createCanvas(mapLenght, mapHeight);
+}
+
+// Draw on canvas; this is called continously
+function draw() {
+  // draw map
+  if (gameInfo != undefined) {
+      for(x = 0; x < gameInfo.board.length; x++) {
+        for (y = 0; y < gameInfo.board.length; y++) {
+            if (gameInfo.board[y][x] == 0) {
+                fill(255, 255, 255);
+                noStroke();
+                rect(x * wallLat, y * wallLat, wallLat, wallLat);
+            } else {
+                fill(0, 29, 178);
+                noStroke();
+                rect(x * wallLat, y * wallLat, wallLat, wallLat);
+            }
+        }
+      }
+  }
+  drawSprites();
+}
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
@@ -124,39 +161,3 @@ document.onkeydown = function(e) {
             break;
     }
 };
-
-function disconnect() {
-    if (stompClient != null) {
-        stompClient.send("/app/bye", {}, JSON.stringify({'content': $("#name").val()}));
-        stompClient.send("/app/end", {}, JSON.stringify({'content': $("#name").val()}));
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function setup() {
-	createCanvas(mapLenght, mapHeight);
-}
-
-function draw() {
-  //background(0,0,0);
-
-  // draw map
-  if (gameInfo != undefined) {
-      fill(0, 29, 178);
-      for(x = 0; x < gameInfo.board.length; x++) {
-        for (y = 0; y < gameInfo.board.length; y++) {
-            if (gameInfo.board[y][x] == 0) {
-                fill(255, 255, 255);
-                rect(x * wallLat, y * wallLat, wallLat, wallLat);
-            } else {
-                fill(0, 29, 178);
-                rect(x * wallLat, y * wallLat, wallLat, wallLat);
-            }
-        }
-      }
-  }
-  drawSprites();
-}
-
